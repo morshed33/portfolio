6dc07192c335d0fbc3a3013fb0e116ac5c89dbe6
devMorshed <ac3m0r@gmail.com>
2023-05-15 21:41:14 +0600

    Init

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<RouterProvider router={router}></RouterProvider>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <p className="text-7xl shadow-2xl m-4 p-10 border rounded-md">Morshed will be here </p>
+    </div>
+  );
+};
+
+export default Home;
