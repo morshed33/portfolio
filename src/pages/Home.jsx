8cbf0e1122f0f42953ac5c39397ceceb75c04cac
devMorshed <ac3m0r@gmail.com>
2023-06-29 19:52:03 +0600

    maintainance

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className='flex items-center justify-center h-screen'>
-      <p className="text-7xl shadow-2xl m-4 p-10 border rounded-md">Morshed will be here </p>
+    <div className='flex text-gray-50 items-center justify-center h-screen bg-sky-950'>
+      {/* <p className="text-7xl shadow-2xl m-4 p-10 border rounded-md">Morshed will be here </p> */}
+      <p>Under Maintainance...</p>
     </div>
   );
 };
